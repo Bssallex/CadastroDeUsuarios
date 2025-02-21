445ef594aac28effeb61e119e9f97f8c857d3229
Alex Bruno <Bssallex@gmail.com>
2025-02-21 15:09:16 -0300

    feat: Adiciona o lombok nas classes como abstração

--- src/main/java/dev/java10x/CadastroDeUsuarios/Tarefas/TarefasModel.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/Tarefas/TarefasModel.java
@@ -2,11 +2,17 @@ package dev.java10x.CadastroDeUsuarios.Tarefas;
 
 import dev.java10x.CadastroDeUsuarios.Usuarios.UsuarioModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_tarefas")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class TarefasModel {
 
--- src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/UsuarioModel.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/UsuarioModel.java
@@ -2,9 +2,15 @@ package dev.java10x.CadastroDeUsuarios.Usuarios;
 
 import dev.java10x.CadastroDeUsuarios.Tarefas.TarefasModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioModel {
 
     @Id
@@ -21,36 +27,4 @@ public class UsuarioModel {
     @JoinColumn(name = "tarefas_id")
     private TarefasModel tarefas;
 
-    public UsuarioModel() {
-    }
-
-    public UsuarioModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
