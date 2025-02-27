ca5c6bcd1ab46ce115eb75ab03aa79c4d17b563b
Alex Bruno <Bssallex@gmail.com>
2025-02-27 17:17:55 -0300

    chore: Inclue a versão do DTO no banco de dados e e mapeia manualmente os caminhos das entidades

--- src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/UsuarioDTO.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/UsuarioDTO.java
@@ -1,4 +1,23 @@
 package dev.java10x.CadastroDeUsuarios.Usuarios;
 
+import dev.java10x.CadastroDeUsuarios.Tarefas.TarefasModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private String cpf;
+    private TarefasModel tarefas;
+
+
+
 }
--- src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/UsuarioMapper.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/UsuarioMapper.java
@@ -1,4 +1,38 @@
 package dev.java10x.CadastroDeUsuarios.Usuarios;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class UsuarioMapper {
+
+    public UsuarioModel map(UsuarioDTO usuarioDTO){
+
+        UsuarioModel usuarioModel = new UsuarioModel();
+        usuarioModel.setId(usuarioDTO.getId());
+        usuarioModel.setNome(usuarioDTO.getNome());
+        usuarioModel.setEmail(usuarioDTO.getEmail());
+        usuarioModel.setImgUrl(usuarioDTO.getImgUrl());
+        usuarioModel.setIdade(usuarioDTO.getIdade());
+        usuarioModel.setCpf(usuarioDTO.getCpf());
+        usuarioModel.setTarefas(usuarioDTO.getTarefas());
+
+        return usuarioModel;
+    }
+
+    public UsuarioDTO map(UsuarioModel usuarioModel){
+
+        UsuarioDTO usuarioDTO = new UsuarioDTO();
+        usuarioDTO.setId(usuarioModel.getId());
+        usuarioDTO.setNome(usuarioModel.getNome());
+        usuarioDTO.setEmail(usuarioModel.getEmail());
+        usuarioDTO.setImgUrl(usuarioModel.getImgUrl());
+        usuarioDTO.setIdade(usuarioModel.getIdade());
+        usuarioDTO.setCpf(usuarioModel.getCpf());
+        usuarioDTO.setTarefas(usuarioModel.getTarefas());
+
+        return usuarioDTO;
+
+    }
+
+
 }
