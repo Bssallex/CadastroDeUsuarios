a26a7d6e097d6c7f96a36e989b97ee7599ec6261
Alex Bruno <Bssallex@gmail.com>
2025-03-05 18:15:38 -0300

    refactor: Refatora o controller para adicionar o ResponseEntity nas rotas HTTP

--- src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/UsuarioController.java
+++ src/main/java/dev/java10x/CadastroDeUsuarios/Usuarios/UsuarioController.java
@@ -1,5 +1,7 @@
 package dev.java10x.CadastroDeUsuarios.Usuarios;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,32 +24,60 @@ public class UsuarioController {
 
     // Adicionar usuarios
     @PostMapping("/criar")
-    public UsuarioDTO criarUsuario(@RequestBody UsuarioDTO usuario){
-       return usuarioService.criarUsuario(usuario);
+    public ResponseEntity<String> criarUsuario(@RequestBody UsuarioDTO usuario){
+        UsuarioDTO usuarioDTO = usuarioService.criarUsuario(usuario);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Usuário criado: " + usuarioDTO.getNome() + " / id: " + usuarioDTO.getId());
     }
 
     // Listar usuarios
     @GetMapping("/listar")
-    public List<UsuarioDTO> listarUsuarios(){
-        return usuarioService.listarUsuarios();
+    public ResponseEntity<List<UsuarioDTO>> listarUsuarios(){
+        List<UsuarioDTO> listaUsuarios = usuarioService.listarUsuarios();
+        return ResponseEntity.ok(listaUsuarios);
     }
 
     // Mostrar todos os usuarios por ID
     @GetMapping("/listar/{id}")
-    public UsuarioDTO listarUsuariosId(@PathVariable Long id){
-        return usuarioService.listarUsuariosId(id);
+    public ResponseEntity<?> listarUsuariosId(@PathVariable Long id){
+
+        UsuarioDTO usuarioID = usuarioService.listarUsuariosId(id);
+
+        if(usuarioID != null){
+            return ResponseEntity.ok(usuarioID);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Não existe nenhum usuário com esse ID");
+        }
     }
 
     // Alterar dados dos usuarios
     @PutMapping("/alterar/{id}")
-    public UsuarioDTO alterarUsuariosId(@PathVariable Long id, @RequestBody UsuarioDTO usuarioAtualizado){
-         return usuarioService.atualizarUsuario(id, usuarioAtualizado);
+    public ResponseEntity<?> alterarUsuariosId(@PathVariable Long id, @RequestBody UsuarioDTO usuarioAtualizado){
+
+        UsuarioDTO alterarUsuarioDTO = usuarioService.atualizarUsuario(id, usuarioAtualizado);
+
+        if(alterarUsuarioDTO != null){
+            return ResponseEntity.ok(usuarioAtualizado);
+        }else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Não existe nenhum usuário com esse ID");
+        }
+
     }
 
     // Deletar usuario por ID
     @DeleteMapping("/deletar/{id}")
-    public void deletarUsuariosId(@PathVariable Long id){
-        usuarioService.deletarUsuarioId(id);
+    public ResponseEntity<String> deletarUsuariosId(@PathVariable Long id){
+
+        if(usuarioService.listarUsuariosId(id) != null){
+            usuarioService.deletarUsuarioId(id);
+            return ResponseEntity.ok("Usuário deletado " + "/ Id: " + id);
+        }
+        else{
+           return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Não existe nenhum usuário com esse ID");
+        }
     }
 
 
